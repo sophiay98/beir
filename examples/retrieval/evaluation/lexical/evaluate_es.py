4c3bfe2399e1f2813071261ed5dd0b607bd698d3
Sophia Yao <sy2743@columbia.edu>
2025-04-01 18:20:47 -0400

    add example eval script for custom es config

---
+++ examples/retrieval/evaluation/lexical/evaluate_es.py
@@ -0,0 +1,133 @@
+"""
+This example show how to evaluate BM25 model (Elasticsearch) in BEIR.
+To be able to run Elasticsearch, you should have it installed locally (on your desktop) along with ``pip install beir``.
+Depending on your OS, you would be able to find how to download Elasticsearch. I like this guide for Ubuntu 18.04 -
+https://linuxize.com/post/how-to-install-elasticsearch-on-ubuntu-18-04/
+For more details, please refer here - https://www.elastic.co/downloads/elasticsearch.
+
+This code doesn't require GPU to run.
+
+If unable to get it running locally, you could try the Google Colab Demo, where we first install elastic search locally and retrieve using BM25
+https://colab.research.google.com/drive/1HfutiEhHMJLXiWGT8pcipxT5L2TpYEdt?usp=sharing#scrollTo=nqotyXuIBPt6
+
+
+Usage: python evaluate_bm25.py
+"""
+
+import logging
+import os
+import pathlib
+import random
+
+from beir import LoggingHandler, util
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.lexical import ESConfigSearch as ESConfigSearch
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
+)
+#### /print debug information to stdout
+
+#### Download scifact.zip dataset and unzip the dataset
+dataset = "scifact"
+url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
+out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
+data_path = util.download_and_unzip(url, out_dir)
+
+#### Provide the data path where scifact has been downloaded and unzipped to the data loader
+# data folder would contain these files:
+# (1) scifact/corpus.jsonl  (format: jsonlines)
+# (2) scifact/queries.jsonl (format: jsonlines)
+# (3) scifact/qrels/test.tsv (format: tsv ("\t"))
+
+corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
+
+#### Lexical Retrieval using Bm25 (Elasticsearch) ####
+#### Provide a hostname (localhost) to connect to ES instance
+#### Define a new index name or use an already existing one.
+#### We use default ES settings for retrieval
+#### https://www.elastic.co/
+
+hostname = "http://localhost:9200"  # localhost
+index_name = "scifact-dfr"  # scifact
+api_key="OVFHX2hwVUJyUkJNNnZvQ0xpMWE6MTRlUWN6UXRRNE9Gd21rTkg4TmQxQQ=="
+#### Intialize ####
+# (1) True - Delete existing index and re-index all documents from scratch
+# (2) False - Load existing index
+initialize = True  # False
+
+#### Sharding ####
+# (1) For datasets with small corpus (datasets ~ < 5k docs) => limit shards = 1
+# SciFact is a relatively small dataset! (limit shards to 1)
+
+similarity_config = {
+  "settings": {
+    "index": {
+      "similarity": {
+        "my_similarity": {
+          "type": "DFR",
+          "basic_model": "g",
+          "after_effect": "l",
+          "normalization": "h2",
+          "normalization.h2.c": "3.0"
+        }
+      }
+    }
+  }
+}
+analyzer_config = {
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "my_custom_analyzer": {
+          "type": "custom", 
+          "tokenizer": "standard",
+          "char_filter": [
+            "html_strip"
+          ],
+          "filter": [
+            "lowercase",
+            "asciifolding"
+          ]
+        }
+      }
+    }
+  }
+}
+
+
+number_of_shards = 1
+model = ESConfigSearch(
+    index_name=index_name,
+    index_config=similarity_config,
+    api_key=api_key,
+    hostname=hostname,
+    initialize=initialize,
+    number_of_shards=number_of_shards,
+
+)
+
+# (2) For datasets with big corpus ==> keep default configuration
+# model = BM25(index_name=index_name, hostname=hostname, initialize=initialize)
+retriever = EvaluateRetrieval(model)
+
+#### Retrieve dense results (format of results is identical to qrels)
+results = retriever.retrieve(corpus, queries)
+
+#### Evaluate your retrieval using NDCG@k, MAP@K ...
+logging.info(f"Retriever evaluation for k in: {retriever.k_values}")
+ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+
+#### Retrieval Example ####
+query_id, scores_dict = random.choice(list(results.items()))
+logging.info(f"Query : {queries[query_id]}\n")
+
+scores = sorted(scores_dict.items(), key=lambda item: item[1], reverse=True)
+for rank in range(10):
+    doc_id = scores[rank][0]
+    logging.info(f"Rank {rank + 1}: {doc_id} [{corpus[doc_id].get('title')}] - {corpus[doc_id].get('text')}\n")
